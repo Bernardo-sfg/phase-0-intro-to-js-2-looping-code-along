fb4708563bfa994eff6e8312ea4655b23e4b221b
Bernardo <service.bsfg@gmail.com>
2024-07-30 09:14:45 -0500

    messing with loops and arrays

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solutions in this file
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. happy birthday to me!`);
+// }
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     debugger;
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+
+
+function writeCards(arrayOfNames, eventName){
+    const messeges = []
+    for (let i = 0; i < arrayOfNames.length; i++) {
+        console.log(`Thank you, ${arrayOfNames[i]}, for the wonderful ${eventName} gift!`)
+        messeges.push(`Thank you, ${arrayOfNames[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return messeges;
+}
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function countDown (number) {
+    while(number >= 0){
+        console.log(number--)
+    }
+}
